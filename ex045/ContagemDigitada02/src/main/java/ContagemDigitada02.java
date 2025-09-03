31f79e1d28a05ba4c1af584f08ba28db1b3ee4bd
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-02 23:13:33 -0300

    ex045

---
+++ ex045/ContagemDigitada02/src/main/java/ContagemDigitada02.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ContagemDigitada02 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Valor inicial da Contagem: ");
+        int valorinicial = scanner.nextInt();
+
+        System.out.print("Valor final: ");
+        int valorFinal = scanner.nextInt();
+
+        System.out.print("Incremento: ");
+        int incremento = scanner.nextInt();
+
+        if (valorinicial < valorFinal) {
+
+            while (valorinicial <= valorFinal) {
+
+                System.out.printf("%d ", valorinicial);
+
+                valorinicial += incremento;
+
+            }
+
+        } else {
+
+            while (valorinicial >= valorFinal) {
+
+                System.out.printf("%d ", valorinicial);
+
+                valorinicial -= incremento;
+
+            }
+
+        }
+
+        System.out.println("Acabou!");
+
+        scanner.close();
+
+    }
+
+}
