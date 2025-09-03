fd1eb8bd4efb23f5149aa3eb44937149461fc126
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-02 22:57:04 -0300

    ex041

---
+++ ex041/Contagem04/src/main/java/Contagem04.java
@@ -0,0 +1,19 @@
+public class Contagem04 {
+
+    public static void main(String[] args) {
+
+        int contador = 100;
+
+        while (contador >= 0) {
+
+            System.out.printf("%d ", contador);
+
+            contador -= 5;
+
+        }
+
+        System.out.println("Acabou!");
+
+    }
+
+}
