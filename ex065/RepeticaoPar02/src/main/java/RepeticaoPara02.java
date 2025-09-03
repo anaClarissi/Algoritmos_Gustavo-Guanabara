93ee86b015b215938a1f7cf7fd4184c85c10afdb
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-03 17:28:43 -0300

    ex065

---
+++ ex065/RepeticaoPar02/src/main/java/RepeticaoPara02.java
@@ -0,0 +1,15 @@
+public class RepeticaoPara02 {
+
+    public static void main(String[] args) {
+
+        for (int contador = 100; contador >= 0; contador -= 10) {
+
+            System.out.printf("%d ", contador);
+
+        }
+
+        System.out.println("Acabou!");
+
+    }
+
+}
