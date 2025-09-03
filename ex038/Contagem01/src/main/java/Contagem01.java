1946100b85d917ccab121dbc7556c89562e1e088
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-02 22:49:00 -0300

    ex038

---
+++ ex038/Contagem01/src/main/java/Contagem01.java
@@ -0,0 +1,19 @@
+public class Contagem01 {
+
+    public static void main(String[] args) {
+
+        int contador = 6;
+
+        while (contador <= 11) {
+
+            System.out.printf("%d ", contador);
+
+            contador++;
+
+        }
+
+        System.out.println("Acabou!");
+
+    }
+
+}
