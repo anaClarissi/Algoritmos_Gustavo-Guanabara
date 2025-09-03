bff21942d6aa091762bf8b4b28fc7a5b7eb9582b
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-02 22:51:48 -0300

    ex039

---
+++ ex039/Contagem02/src/main/java/Contagem02.java
@@ -0,0 +1,19 @@
+public class Contagem02 {
+
+    public static void main(String[] args) {
+
+        int contador = 10;
+
+        while (contador >= 3) {
+
+            System.out.printf("%d ", contador);
+
+            contador--;
+
+        }
+
+        System.out.println("Acabou!");
+
+    }
+
+}
