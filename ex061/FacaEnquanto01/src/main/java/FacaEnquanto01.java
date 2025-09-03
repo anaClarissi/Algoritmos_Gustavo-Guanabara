615203ef74eeea8171f840db9817c000629cb637
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-03 17:06:18 -0300

    ex061

---
+++ ex061/FacaEnquanto01/src/main/java/FacaEnquanto01.java
@@ -0,0 +1,17 @@
+public class FacaEnquanto01 {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        do {
+
+            System.out.printf("%d ",contador);
+
+            contador += 3;
+
+        } while (contador <= 30);
+
+    }
+
+}
