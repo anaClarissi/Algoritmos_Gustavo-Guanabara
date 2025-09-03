ac1ebeb9abe8e3191f0867665c3d1d3e0b134d52
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-02 22:54:07 -0300

    ex040

---
+++ ex040/Contagem03/src/main/java/Contagem03.java
@@ -0,0 +1,19 @@
+public class Contagem03 {
+
+    public static void main(String[] args) {
+
+        int contador = 0;
+
+        while (contador <= 18) {
+
+            System.out.printf("%d ", contador);
+
+            contador += 3;
+
+        }
+
+        System.out.println("Acabou!");
+
+    }
+
+}
