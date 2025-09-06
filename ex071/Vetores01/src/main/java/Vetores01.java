a648e7ef87d417c9a1336614383ac9e988a59417
Ana Clarissi <anaclarissicruz@gmail.com>
2025-09-06 20:17:34 -0300

    ex071

---
+++ ex071/Vetores01/src/main/java/Vetores01.java
@@ -0,0 +1,17 @@
+public class Vetores01 {
+
+    public static void main(String[] args) {
+
+        int[] valores = new int[8];
+
+        for (int contador = 0; contador < valores.length; contador++) {
+
+            valores[contador] = 999;
+
+            System.out.printf("%d ", valores[contador]);
+
+        }
+
+    }
+
+}
